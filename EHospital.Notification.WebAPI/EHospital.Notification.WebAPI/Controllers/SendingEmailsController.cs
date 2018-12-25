73202604a0ebda58827fd0e51108dd7767ed4f3a
Julia Kropivnaya <kropisha@gmail.com>
2018-12-26 00:52:02 +0200

    add worked version

--- EHospital.Notification.WebAPI/EHospital.Notification.WebAPI/Controllers/SendingEmailsController.cs
+++ EHospital.Notification.WebAPI/EHospital.Notification.WebAPI/Controllers/SendingEmailsController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using EHospital.Notification.BusinessLogic;
+using EHospital.Notification.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EHospital.Notification.WebAPI.Controllers
@@ -10,36 +9,18 @@ namespace EHospital.Notification.WebAPI.Controllers
     [ApiController]
     public class SendingEmailsController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> SendMessage([FromBody]Email body)
         {
+            string email = body.email;
+            string topic = body.topic;
+            string text = body.text;
+
+            EmailService emailService = new EmailService();
+            await emailService.SendEmailAsync(email, topic, text);
+            return Ok();
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
